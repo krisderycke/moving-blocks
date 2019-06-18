cdf8740733144a1b6bfb5b9b52e929389001fbfa
Kris de Rycke <derycke.kris@gmail.com>
2019-06-18 14:12:32 +0200

    right corresponding keyCode functions

--- index.js
+++ index.js
@@ -1,4 +1,32 @@
-document.onkeydown = myMove();
+document.onkeydown = myMove;
 
 let animal = document.getElementById("animal");
 let animalLeft = 0;
+let animalTop = 0;
+
+//keycodes are: left(37) right (39) up(38) down(40)
+function myMove(e) {
+  if (e.keyCode == 39) {
+    //makes block go right
+    animalLeft += 5;
+    animal.style.left = animalLeft + "px";
+  }
+
+  if (e.keyCode == 37) {
+    //makes block go left
+    animalLeft -= 5;
+    animal.style.left = animalLeft + "px";
+  }
+
+  if (e.keyCode == 40) {
+    //makes block go down
+    animalTop += 5;
+    animal.style.top = animalTop + "px";
+  }
+
+  if (e.keyCode == 38) {
+    // makes block go up
+    animalTop -= 5;
+    animal.style.top = animalTop + "px";
+  }
+}
