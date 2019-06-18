bfe044b68b4a906121ce57a448a8f8ab477717b3
Kris de Rycke <derycke.kris@gmail.com>
2019-06-18 14:36:23 +0200

    set window boundries by using if conditions inside e.keyCodes

--- index.js
+++ index.js
@@ -10,23 +10,39 @@ function myMove(e) {
     //makes block go right
     animalLeft += 5;
     animal.style.left = animalLeft + "px";
+    if (animalLeft >= 1266) {
+      animalLeft -= 5;
+      console.log("max right");
+    }
   }
 
   if (e.keyCode == 37) {
     //makes block go left
     animalLeft -= 5;
     animal.style.left = animalLeft + "px";
+    if (animalLeft <= 1) {
+      animalLeft += 5;
+      console.log("max left");
+    }
   }
 
   if (e.keyCode == 40) {
     //makes block go down
     animalTop += 5;
     animal.style.top = animalTop + "px";
+    if (animalTop >= 525) {
+      animalTop -= 5;
+      console.log("max bottom");
+    }
   }
 
   if (e.keyCode == 38) {
     // makes block go up
     animalTop -= 5;
     animal.style.top = animalTop + "px";
+    if (animalTop <= 1) {
+      animalTop += 5;
+      console.log("max top");
+    }
   }
 }
