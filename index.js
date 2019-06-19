0f7074478da11d1043484f4ae2e2e444cf35966c
Kris de Rycke <derycke.kris@gmail.com>
2019-06-19 09:59:07 +0200

    change MAth.random so that position is always a multiple of 10(px)

--- index.js
+++ index.js
@@ -4,8 +4,6 @@ let animal = document.getElementById("animal");
 let animalLeft = 0;
 let animalTop = 0;
 let food = document.getElementById("food");
-let foodLeft = 0;
-let foodTop = 0;
 
 //keycodes are: left(37) right (39) up(38) down(40)
 function myMove(e) {
@@ -34,7 +32,7 @@ function myMove(e) {
     //makes block go down
     animalTop += 10;
     animal.style.top = animalTop + "px";
-    if (animalTop >= window.innerHeight - 100) {
+    if (animalTop >= window.innerHeight - 104) {
       animalTop -= 10;
       console.log("max bottom");
     }
@@ -59,11 +57,19 @@ function mySpawn() {
   let maxHeight = window.innerHeight - food.offsetHeight;
   console.log("exc function mySpawn");
   console.log(maxWidth, maxHeight);
-  let randomWidth = Math.random() * maxWidth;
-  let randomHeight = Math.random() * maxHeight;
+  let randomWidth = Math.floor((Math.random() * maxWidth) / 5) * 2; //gedeeld door 5,maal 2: om een even getal uit te komen in stapjes van 10px
+  let randomHeight = Math.floor((Math.random() * maxHeight) / 5) * 2;
   console.log(randomHeight, randomWidth);
   food.style.left = randomWidth + "px";
   food.style.top = randomHeight + "px";
   console.log("poing");
-  food.innerHTML = "poing";
+}
+
+//check whether moving block has same position as food
+
+document.onkeyup = samePosition;
+function samePosition() {
+  if (food.style.left === animal.style.left) {
+    alert("gotcha!");
+  }
 }
